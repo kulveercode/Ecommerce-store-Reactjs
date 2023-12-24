930a407a7c574b95f4571a8f9c9dfa7e876f51b5
Kulveer Singh <kulveer.coder@gmail.com>
2023-12-24 19:57:26 +0530

    cart done

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -10,6 +10,7 @@ export default function Cart() {
           img: "https://assets.ajio.com/medias/sys_master/root/20231130/bqsp/65679678ddf7791519a5b0ea/-473Wx593H-466837218-green-MODEL.jpg",
           img2: "https://assets.ajio.com/medias/sys_master/root/20231130/hLw6/65679678ddf7791519a5b073/-473Wx593H-466837218-green-MODEL6.jpg",
           title: "Men Quilted Regular Fit Puffer Jacket",
+          desc: "Men winter puffed Jacket",
           isNew: true,
           oldPrice: 19,
           Price: 12,
@@ -19,6 +20,7 @@ export default function Cart() {
           img: "https://assets.ajio.com/medias/sys_master/root/20231130/bqsp/65679678ddf7791519a5b0ea/-473Wx593H-466837218-green-MODEL.jpg",
           img2: "https://assets.ajio.com/medias/sys_master/root/20231130/hLw6/65679678ddf7791519a5b073/-473Wx593H-466837218-green-MODEL6.jpg",
           title: "Men Quilted Regular Fit Puffer Jacket",
+          desc: "Men winter puffed Jacket",
           isNew: true,
           oldPrice: 50,
           Price: 25,
@@ -32,12 +34,18 @@ export default function Cart() {
             <img src={item.img} alt="" />
             <div className="details">
                 <h1>{item.title}</h1>
-                <p>{item.desc.substring(0, 100)}</p>
+                <p>{item.desc?.substring(0, 100)}</p>
                 <div className="price">1 X ${item.Price}</div>
             </div>
         <DeleteOutlinedIcon className='delete' />
         </div>
       ))}
+      <div className="total">
+        <span>SUBTOTAL</span>
+        <span>$123</span>
+      </div>
+      <button>PROCEED TO CHECKOUT</button>
+      <span className='reset'>Reset Cart</span>
     </div>
   )
 }
